8bcae2ff08813fb02ebd634cabb1fdcbf1b2479d
michael-2956 <work.michael.2956@gmail.com>
2021-01-02 21:48:20 +0200

    Add the vertex formatting features and rename technical functions

--- graph_io.py
+++ graph_io.py
@@ -2,58 +2,59 @@
 This module contains functions that implement the ability
 to read and write info to file, build adjacency dictionary and
 adjacency matrix.
+
+FEEL FREE TO MAKE CHANGES AND TO IMPROVE THIS FILE
 '''
 
 from typing import Dict, Tuple, Set
-from itertools import chain
 import numpy as np
 
-def read_file(path_to_file: str) -> Set[Tuple[int]]:
-    """
+def _read_file(path_to_file: str) -> Tuple[int, Set[Tuple[int]]]:
+    '''
     This function reads info from file containing a graph.
     The file should contain multiple following lines for each edge:
         int (initial vertex num), int (terminal vertex num)
 
     Returns a set of tuples containing these edges.
-    """
+    '''
 
     with open(path_to_file, encoding='utf-8') as file:
-        # if you assume that there will be the first line
-        # you can simply write file.next() (because files
-        # are actually iterators)
         data = file.readlines()
 
-    # doesn't get your idea to count vertices in function,
-    # that reads the file, if you need this, write other function
-    # (you might use itertools.chain for this, or some comprehension)
-    #
-    # number_of_vertices = 0
-    # data_set = set()
+    raw_data_set = set()
+    unique_points_set = set()
 
-    return {tuple(map(int, line.rstrip().split(','))) for line in data}
-    #for idx, line in enumerate(data):
-        #initial, terminal = map(int, line.rstrip().split(','))
-        #number_of_vertices = max(number_of_vertices, initial, terminal)
-        #data_set.add((initial, terminal))
-#
-    #return number_of_vertices, data_set
+    for line in data[1:]:
+        initial, terminal = map(int, line.rstrip().split(','))
+        unique_points_set.add(initial)
+        unique_points_set.add(terminal)
+        raw_data_set.add((initial, terminal))
+    
+    number_of_vertices = len(unique_points_set)
+
+    point_rename_map = {
+        old_id:new_id
+        for old_id, new_id in zip(
+            list(unique_points_set),
+            list(range(number_of_vertices))
+        )
+    }
+
+    data_set = set()
+    for initial, terminal in raw_data_set:
+        data_set.add((point_rename_map[initial], point_rename_map[terminal]))
+
+    return number_of_vertices, data_set
 
 
-def count_vertices(set_of_edges: Set[int]) -> int:
-    """
-    Counts edges of graph
-    """
-
-    return len(set(chain(*set_of_edges)))
-
-def adjacency_dict(graph_data: Set[Tuple[int]], oriented: bool=False) -> Dict[int, Set[int]]:
-    """
+def _adjacency_dict(graph_data: Set[Tuple[int]], oriented: bool=False) -> Dict[int, Set[int]]:
+    '''
     This function reads info from file containing a graph and
     creates an adjacency dictionary, where keys are vertices
     of the graph and values are their adjacent ones.
     The file should contain multiple following lines for each edge:
         int (initial vertex num), int (terminal vertex num)
-    """
+    '''
 
     graph = {}
     for (initial, terminal) in graph_data:
@@ -63,20 +64,19 @@ def adjacency_dict(graph_data: Set[Tuple[int]], oriented: bool=False) -> Dict[in
 
     return graph
 
-# NO -> need to be rewritten, because I changed the first function
-# doesn't like this thou
-def adjacency_matrix(number_of_vertices: int, graph_data: Set[Tuple[int]],
-                    oriented=False) -> np.array:
-    """
+
+def _adjacency_matrix(
+        number_of_vertices: int, graph_data: Set[Tuple[int]], oriented=False
+    ) -> np.array:
+    '''
     Build an adjacency matrix for a graph
 
     Args: edges: set of tuples, that represent edges of the graph
 
     Returns: n*n matrix, matrix[i][j] == 1, if there is such an edge
     and == 0, if there is not.
-    """
+    '''
 
-    # I don't think that this is efficient enough to work with such an ugly relisation
     matrix = np.zeros((number_of_vertices+1, number_of_vertices+1), dtype=bool)
     for (initial, terminal) in graph_data:
         matrix[initial, terminal] = True
@@ -90,17 +90,14 @@ def read_adjacency_dict(path_to_file: str, oriented: bool=False) -> Dict[int, Se
     """
     Reads a graph from file and forms an adjacency dict
     """
-
-    # that's good, but what for?
-    return adjacency_dict(read_file(path_to_file)[1], oriented=oriented)
+    return _adjacency_dict(_read_file(path_to_file)[1], oriented=oriented)
 
 
 def read_adjacency_matrix(path_to_file: str, oriented: bool=False) -> np.array:
     """
     Reads a graph from file and forms an adjacency matrix
     """
-    # that's good, but what for?
-    return adjacency_matrix(*read_file(path_to_file), oriented=oriented)
+    return _adjacency_matrix(*_read_file(path_to_file), oriented=oriented)
 
 
 def write_adjacency_dict(
@@ -155,20 +152,20 @@ def write_adjacency_matrix(
                     )
 
 
-def main():
+def _main():
     """
-    An interactive function of the module.
+    An interactive function of the module, demonstrating the main functions
     """
 
     write_adjacency_dict(
-            read_adjacency_dict('graph.csv'),
-            "graph_out_dict.csv",
+            read_adjacency_dict('graph.csv', oriented=True),
+            "graph_out_dict.csv", oriented=True
     )
     write_adjacency_matrix(
-            read_adjacency_matrix('graph.csv'),
-            "graph_out_matrix.csv",
+            read_adjacency_matrix('graph.csv', oriented=True),
+            "graph_out_matrix.csv", oriented=True
     )
 
 
 if __name__ == "__main__":
-    main()
+    _main()
